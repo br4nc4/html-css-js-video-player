13a989746dbe47a8fb60ade41e93997789e76eb0
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-10-06 15:55:06 +0200

    second function play pause button change icon

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ playPauseBtn.addEventListener("click", () => {
     //se il video è in pausa, avvia il video altrimenti metti in pausa il video
     mainVideo.paused ? mainVideo.play() : mainVideo.pause();
 });
+
+mainVideo.addEventListener("play", () => {
+    // se il video è avviato sostituisce l'icona play con l'icona pause
+    playPauseBtn.classList.replace("fa-play", "fa-pause");
+});
+
+mainVideo.addEventListener("pause", () => {
+    // se il video è in pausa sostituisce l'icona pause con l'cona play
+    playPauseBtn.classList.replace("fa-pause", "fa-play");
+});
